108addca3c674be4fcf61cd5f26b00840ef56a75
MHubertus <108489700+MHubertus@users.noreply.github.com>
2022-07-02 11:19:18 +0200

    Delete kaschusoHelper.js

--- kaschusoHelper.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("nav_top_header").innerHTML="<img alt=\"top logo\" src=\"https://i.imgur.com/DN71Axu.png\" border=\"0\">"
-document.getElementById("nav_top_corner_cell_upper").remove
